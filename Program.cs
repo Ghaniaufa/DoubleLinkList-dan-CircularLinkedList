30415958cb7c07eb45a053138dda12a79b986167
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2022-11-18 20:37:39 +0700

    continues the method before for the chechk the values entered

--- Program.cs
+++ Program.cs
@@ -240,6 +240,10 @@ namespace double_linked_list
                             break;
                     }
                 }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Check for the values entered");
+                }
             }
         }
     }
