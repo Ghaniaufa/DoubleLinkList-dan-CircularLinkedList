40b7bbdc5bfac1c38f211c4afef784b2ea5d66f2
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2022-11-18 20:28:00 +0700

    add case 2 for obj.listEmpty

--- Program.cs
+++ Program.cs
@@ -184,14 +184,26 @@ namespace double_linked_list
                                 obj.addNode();
                             }
                             break;
+                        case '2':
+                            {
+                                if (obj.listEmpty())
+                                {
+                                    Console.WriteLine("\nList is empty");
+                                    break;
+                                }
+                                Console.Write("\nEnter the roll number of the student" + "Whose record is to be deleted: ");
+                                int rollNo = Convert.ToInt32(Console.ReadLine());
+                                Console.WriteLine();
+                                if (obj.dellNode(rollNo) == false)
+                                    Console.WriteLine("Record not found");
+                                else
+                                Console.WriteLine("Record with roll number " + rollNo + "deleted \n")
+                            }
+                            break;
+
                     }
                 }
             }
-            
-             
-            
-
-
         }
     }
 } 
