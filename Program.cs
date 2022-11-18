4c52cac9c214de62b6705b878420ddc45f63f71e
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2022-11-18 16:48:49 +0700

    Create Class Node

--- Program.cs
+++ Program.cs
@@ -3,4 +3,11 @@
 namespace double_linked_list
 {
     class Node
+    {
+        /* Node class represent the node of doubly linked list
+         * It consists of the information part and links to
+         * * Its succeding and preceeding
+         * * In terms of next and previous*/
+        
+    }
 }
